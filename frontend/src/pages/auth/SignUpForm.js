21e4b76db17ead3490c61b0d4319c0c9b6929bfc
KeyWarden <ekeeley9@gmail.com>
2023-08-31 08:08:40 +0000

    adjust dj-rest-auth

--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -1,33 +1,101 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Col,
+  Container,
+  Alert,
+} from "react-bootstrap";
+import axios from "axios";
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: '',
+    password: '',
+    passwordConfirm: ''
+  });
+  const { username, password, passwordConfirm } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value
+    })
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post('/dj-rest-auth/registration/', signUpData)
+      history.push('/signin')
+    } catch(err) {
+      setErrors(err.response?.data)
+    }
+  };
+
   return (
     <div>
         <Container className={`${styles.Content}`}>
             <h1 className={styles.Header}>sign up</h1>
 
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="username">
                     <Form.Label className="d-none">username</Form.Label>
-                    <Form.Control className={styles.Input} type="email" placeholder="Username" name="username" />
+                    <Form.Control 
+                      className={styles.Input} 
+                      type="text" 
+                      placeholder="Username" 
+                      name="username"
+                      value={username}
+                      onChange={handleChange}
+                    />
                 </Form.Group>
+                {errors.username?.map((message, idx) =>
+                  <Alert variant="warning" key={idx}>{message}</Alert>
+                )}
                 <Form.Group controlId="password">
                     <Form.Label className="d-none">password</Form.Label>
-                    <Form.Control className={styles.Input} type="password" placeholder="Password" name="password" />
+                    <Form.Control
+                      className={styles.Input}
+                      type="password"
+                      placeholder="Password"
+                      name="password"
+                      value={password}
+                      onChange={handleChange}
+                    />
                 </Form.Group>
+                {errors.password?.map((message, idx) =>
+                  <Alert variant="warning" key={idx}>{message}</Alert>
+                )}
                 <Form.Group controlId="passwordConfirm">
                     <Form.Label className="d-none">confirm password</Form.Label>
-                    <Form.Control className={styles.Input} type="password" placeholder="Confirm Password" name="passwordConfirm" />
+                    <Form.Control
+                      className={styles.Input}
+                      type="password"
+                      placeholder="Confirm Password"
+                      name="passwordConfirm"
+                      value={passwordConfirm}
+                      onChange={handleChange}
+                    />
                 </Form.Group>
+                {errors.passwordConfirm?.map((message, idx) =>
+                  <Alert variant="warning" key={idx}>{message}</Alert>
+                )}
                 <Button className={`${btnStyles.Button}`} variant="primary" type="submit">
                     Sign Up
                 </Button>
+                {errors.non_field_errors?.map((message, idx) =>
+                  <Alert variant="warning" key={idx} className="mt-3">{message}</Alert>
+                )}
             </Form>
 
         </Container>
